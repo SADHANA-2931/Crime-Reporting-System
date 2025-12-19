39281463b5c2b32fb370f019f846b82e4bfce229
Sadhana T R <sadhanatr2@gmail.com>
2025-12-19 19:49:47 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -149,7 +149,7 @@ if(isset($_POST['s'])){
 					<p style="color:#dfdfdf">Full Name</p><input type="text"  name="name"  required="" id="name1" onfocusout="f1()" />
 					<p style="color:#dfdfdf">Email-Id</p><input type="email"  name="email"  required="" id="email1" onfocusout="f1()"/>
                     <p style="color:#dfdfdf">Password</p><input type="text"  name="password"  placeholder="6 Character minimum" pattern=".{6,}" id="pass" onfocusout="f1()"/>
-					<p style="color:#dfdfdf">Home Adress</p><input type="text"  name="adress"  required="" id="addr" onfocusout="f1()"/>
+					<p style="color:#dfdfdf">Home Address</p><input type="text"  name="address"  required="" id="addr" onfocusout="f1()"/>
 					<p style="color:#dfdfdf">Aadhar Number</p><input type="text"  name="aadhar_number" minlength="12" maxlength="12" required pattern="[123456789][0-9]{11}" id="aadh" onfocusout="f1()"/>
 					<div class="left-w3-agile">
 						<p style="color:#dfdfdf">Gender</p><select class="form-control" name="gender">
